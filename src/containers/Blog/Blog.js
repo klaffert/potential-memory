1cb73c534bfe9a928c9f3fc4b89d671ae473db75
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2020-02-24 20:35:14 -0800

    setting up links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -22,24 +22,20 @@ class Blog extends Component {
         const updatedPosts = posts.map(post => {
           return {
             ...post,
-            author: 'Kelliann'
+            author: 'Max'
           };
         });
-        this.setState({
-          posts: updatedPosts
-        });
+        this.setState({ posts: updatedPosts });
+        // console.log( response );
       })
       .catch(error => {
-        this.setState({
-          error: true
-        });
+        // console.log(error);
+        this.setState({ error: true });
       });
   }
 
   postSelectedHandler = id => {
-    this.setState({
-      selectedPostId: id
-    });
+    this.setState({ selectedPostId: id });
   };
 
   render() {
@@ -51,15 +47,26 @@ class Blog extends Component {
             key={post.id}
             title={post.title}
             author={post.author}
-            clicked={() => {
-              this.postSelectedHandler(post.id);
-            }}
+            clicked={() => this.postSelectedHandler(post.id)}
           />
         );
       });
     }
+
     return (
-      <div>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <section className="Posts">{posts}</section>
         <section>
           <FullPost id={this.state.selectedPostId} />
