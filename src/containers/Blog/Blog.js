32398d9bbd8484b3a2233dc712c4b341aec06417
Kelliann Lafferty <lafferty.kelliann@gmail.com>
2020-02-26 13:59:47 -0800

    rendering components for routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -20,8 +20,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route exact path="/" render={() => <h1>Home</h1>} />
-        <Route path="/" render={() => <h1>Home 2</h1>} />
+        {/* <Route exact path="/" render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+        <Route path="/" exact component={Posts} />
       </div>
     );
   }
